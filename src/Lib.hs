02c259923850b535f1cd504929c9eaf35093f847
Robert Pearce <me@robertwpearce.com>
2017-09-15 16:30:59 +1200

    Remove until I have something solid

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,63 +3,7 @@ module Lib
     ) where
 
 import System.IO
-import System.Directory (removeFile, renameFile)
-import System.Environment (getArgs, lookupEnv)
-import Data.Maybe (fromMaybe)
-{-import Data.List.Split (splitOn)-}
-{-import Data.Time-}
 
 timeTrack :: IO ()
-timeTrack = do
-    args <- getArgs
-    case args of
-        ["add", date, text] -> add date text
-        ["rm", n]           -> putStrLn $ "Remove line " ++ n ++ " (COMING SOON)"
-        ["ls"]              -> ls
-        otherwise           -> putStrLn "Please either use `add`, `rm` or `ls`"
-
-defaultPath :: String
-defaultPath =
-    "/Users/rpearce/Dropbox/timetrack.txt"
-
-add :: String -> String -> IO ()
-add date text = do
-    filePath <- fromMaybe defaultPath <$> lookupEnv "TIMETRACK_PATH"
-    rows <- readLines filePath
-    let
-        tmpFilePath :: String
-        tmpFilePath = filePath ++ ".tmp"
-
-        newLine :: String
-        newLine = show (nextRowNum rows) ++ ".." ++ date ++ ".." ++ text
-
-        updatedRows :: [String]
-        updatedRows = rows ++ [newLine]
-
-    writeFile tmpFilePath $ unlines updatedRows
-    removeFile filePath
-    renameFile tmpFilePath filePath
-    putStrLn $ "=> Wrote to " ++ filePath
-    putStrLn $ indentedOutput newLine
-
-ls :: IO ()
-ls = do
-    filePath <- fromMaybe defaultPath <$> lookupEnv "TIMETRACK_PATH"
-    rows <- readLines filePath
-    putStrLn $ unlines rows
-
-readLines :: FilePath -> IO [String]
-readLines =
-    fmap lines . readFile
-
-nextRowNum :: [String] -> Integer
-nextRowNum =
-    inc . fromIntegral . length
-
-inc :: Integer -> Integer
-inc n =
-    n + 1
-
-indentedOutput :: String -> String
-indentedOutput str =
-    "   ├── " ++ str
+timeTrack =
+    putStrLn "Coming soon"
