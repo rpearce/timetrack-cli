32344a3c9c1df84169aa098b9c237080d711861a
Robert Pearce <me@robertwpearce.com>
2018-10-24 11:41:54 +1300

    Update stack resolver and fix pendantic issues in Update

--- src/Commands/Update.hs
+++ src/Commands/Update.hs
@@ -5,10 +5,6 @@ module Commands.Update
 
 import           Commands.Add    (add)
 import           Commands.Remove (rm)
-import qualified Entry           as E
-import           Helpers         (getFilePath, indentedOutput,
-                                  updateFileContent)
-import           Text.Read       (readMaybe)
 
 
 forHelp :: String
@@ -21,19 +17,9 @@ noArgs =
     "No arguments provided.\n" ++ forHelp
 
 
-lineNotFound :: String
-lineNotFound =
-    "Line not found.\n" ++ forHelp
-
-
 usage :: String
 usage =
     "Usage: timetrack update <LINE NUMBER> <DATE> <MESSAGE>"
-
-
-validLineNum :: String
-validLineNum =
-    "Invalid line number provided.\n" ++ forHelp
 
 
 update :: [String] -> IO String
@@ -47,6 +33,5 @@ update _                  = return usage
 -- @TODO: safely do all this
 handleUpdate :: (String, String, String) -> IO String
 handleUpdate (n, date, message) = do
-    path <- getFilePath
     _ <- rm [n]
     add [date, message]
