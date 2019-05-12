738b081d012cec70599dc1c5ae95de8e7561ba15
Robert Pearce <me@robertwpearce.com>
2019-05-13 11:28:34 +1200

    nixify project

--- Makefile
+++ Makefile
@@ -1,2 +1,14 @@
+build:
+	nix-build --attr project release.nix
+
+cabal:
+	nix-shell --pure -p cabal2nix --run "cabal2nix ." > project.nix
+
+repl:
+	nix-shell --pure --run "cabal new-repl"
+
 run:
-	stack build --pedantic --copy-bins
+	result/bin/timetrack-cli
+
+test:
+	nix-shell --pure --run "cabal new-test all"
---
+++ release.nix
@@ -0,0 +1,45 @@
+{ compiler ? "ghc865"
+, pkgs ? import ./pkgs.nix
+}:
+
+let
+  config = {
+    packageOverrides = pkgs: rec {
+      haskell = pkgs.haskell // {
+        packages = pkgs.haskell.packages // {
+          "${compiler}" = pkgs.haskell.packages."${compiler}".override {
+            overrides = haskellPackagesNew: haskellPackagesOld: rec {
+              project = haskellPackagesNew.callPackage ./project.nix { };
+            };
+          };
+        };
+      };
+    };
+  };
+
+  # Pinned nixpkgs
+  bootstrap = import <nixpkgs> { };
+  nixpkgs = builtins.fromJSON (builtins.readFile ./nixpkgs.json);
+  src = bootstrap.fetchFromGitHub {
+    owner = "NixOS";
+    repo  = "nixpkgs";
+    inherit (nixpkgs) rev sha256;
+  };
+  pkgs = import src { inherit config; };
+  haskellPackages = pkgs.haskell.packages.${compiler};
+in
+  {
+    project = haskellPackages.project;
+    shell = haskellPackages.shellFor {
+      packages = p: with p; [
+        project
+      ];
+      buildInputs = with haskellPackages; [
+        cabal-install
+        ghcid
+        hlint
+        stylish-haskell
+      ];
+      withHoogle = true;
+    };
+  }
