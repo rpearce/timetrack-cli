af0df12d67e6d3282ae9ac42eff8c0d8a4119f11
Robert Pearce <me@robertwpearce.com>
2019-05-20 07:48:57 +1200

    v0.1.0.1 info

--- project.nix
+++ project.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, directory, stdenv }:
 mkDerivation {
   pname = "timetrack-cli";
-  version = "0.1.0.0";
+  version = "0.1.0.1";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
